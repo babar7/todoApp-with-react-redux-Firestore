265d7ece263743af132893220a0bfce09c8e95dd
babar7 <babarshair97@gmail.com>
2018-09-23 22:34:10 +0500

    Components Root

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import SigninComponent from './Signin/index';
+import SignupComponent from './Signup/index';
+import DashBoardComponent from './Dashboard/index';
+
+export {
+    SigninComponent,
+    SignupComponent,
+    DashBoardComponent
+};
